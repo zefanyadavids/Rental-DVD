35f530eb8de1b7eb87dde4cb9cb700b5f32b697f
Stolzee <38659093+Stolzee@users.noreply.github.com>
2018-04-23 21:51:51 +0700

    Update Laporan

---
+++ laporan-peminjaman.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Laporan</title>
+	<link rel="stylesheet" type="text/css" href="css/style-head-side.css">
+	<link rel="stylesheet" type="text/css" href="css/style-laporan.css">
+	<link rel="stylesheet" type="text/css" href="css/style-content.css">
+	<link rel="stylesheet" href="css/fontawesome-all.css">
+	<script src="js/utill.js"></script>
+</head>
+<body>
+
+	<?php 
+		include "navigasi.php"; 
+		include "side.php";
+	?>
+
+	<div id="content">
+	<?php
+		include "navigasi-laporan.php";
+	?>
+	<table class="table">
+		<tr>
+			<th>No.</th>
+			<th>Kode Detail Transaksi</th>
+			<th>ID Member</th>
+			<th>ID Karyawan</th>
+			<th>Tanggal Peminjaman</th>
+			<th>Kode DVD</th>
+			<th>Tanggal Kembali Normal</th>
+			<th>Biaya</th>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</table>
+	</div>
+
+</body>
+</html>
