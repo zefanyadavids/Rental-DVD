425bda450ec5480aa4269784c3e9aa507fe209d7
yogarizky <yoga.rizky@ti.ukdw.ac.id>
2018-04-27 00:39:55 +0700

    Penambahan USER MODE

---
+++ user/user-side.php
@@ -0,0 +1,19 @@
+<div id="side">
+	<div id="profil">
+		<h3>Welcome, User</h3>
+		<img src="img/profil.png" class="circlepic">
+		<p> </p>
+		<p>USER MODE</p>
+	</div>
+	<div id="time">
+		<div class="timeWidget">
+			<p class="printTime"></p>
+		</div>
+		<div class="timeWidget">
+			<p class="printTime"></p>
+			<p class="printTime"></p>
+			<p class="printTime"></p>
+		</div>
+	</div>
+</div>
+<script src="js/effect.js"></script>
